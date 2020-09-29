1ddc3c3e93b50ed62a76c4f9dcc4aa2c10b8b167
Stefan Tobler <toblerlstefan@gmail.com>
2020-09-29 00:26:32 -0400

    rebuilding site Tue Sep 29 00:26:32 EDT 2020

--- en.search.min.3636c74c3ce8f6ebe5b8702c29c2c1a1948fb0144fa0a6078f6c15334dbed06d.js
+++ en.search.min.3636c74c3ce8f6ebe5b8702c29c2c1a1948fb0144fa0a6078f6c15334dbed06d.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c43ed27f26e160ad73e0b1767121d0ad7c5ca95e386410d6f41f3a65d36e59f0.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.a6ed6448ca436f51f683a7ac5d83db83161d28fa90386e8f3b3eed64949491b2.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
